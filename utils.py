af6bbddbe6012b6230aed2de085e09928b836db3
luizhenriquesousad1-creator <luizhenriquesousad1@gmail.com>
2026-04-17 12:08:08 -0300

    Implement file selection and result display functions
    Added functions for file selection and result presentation.

--- utils.py
+++ utils.py
@@ -1,1 +1,15 @@
+import pandas as pd
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+def organiza():
+  print("-" *80)
+
+def apresenta_resultado(produto):
+  return (f"Oproduto mais vendido foi {produto['nome']} que vendeu {produto['quantidade']} unidades.")
+
+def selecionar_arquivo():
+  Tk().wothdraw()
+  caminho = askopenfilename(title="Selecione o arquivo CSV", filetype=[("arquivo CSV", "*.csv")])
+  return caminho
 
