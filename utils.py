3fe2f4fef5ea0b5714e96cfe43a0e8c9d96fa864
luizhenriquesousad1-creator <luizhenriquesousad1@gmail.com>
2026-04-17 13:46:15 -0300

    Implement data loading function with error handling
    Add function to load CSV data and handle errors

--- utils.py
+++ utils.py
@@ -13,3 +13,19 @@ def selecionar_arquivo():
   caminho = askopenfilename(title="Selecione o arquivo CSV", filetype=[("arquivo CSV", "*.csv")])
   return caminho
 
+def carrega_dados():
+  try:
+    caminho_vendas = selecionar_arquivos()
+    caminho_produto = selecionar_arquivo()
+    caminho_categoria = selecionar_arquivo()
+
+    df_vendas = pd.read_csv(caminho_vendas)
+    df_produto = pd.read_csv(caminho_produto)
+    df_categoria = pd.read_csv(caminho_categoria)
+    
+    return df_vendas, df_produtos, df_categoria
+
+  except Exception as e:
+    print("Erro ao carregar arquivos. Verifique se selecionou arquivos CSV válidos.")
+    print(f"Erro ao carregar arquivo: {e}")
+    return None
